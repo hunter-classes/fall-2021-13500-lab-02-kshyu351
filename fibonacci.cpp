d38bb9bbd8dd20e4eacac41db3835bdbf5e215ea
Kirsten Shyu <kirsten.shyu00@myhunter.cuny.edu>
2021-09-11 17:41:23 -0400

    add fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,29 @@
+/*
+Author: Kirsten Shyu
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 2D
+
+Write a program fibonacci.cpp, which uses an
+ array of ints to compute and print all 
+ Fibonacci numbers from F(0) to F(59).
+*/
+
+#include <iostream>
+
+int main() {
+
+    int fib[60]; 
+    fib[0] = 0;
+    fib[1] = 1;
+    std::cout << fib[0] << std::endl;
+     std::cout << fib[1] << std::endl;
+
+    for(int i = 2; i <= 59; i++) { 
+        
+        fib[i] = fib[i-1] + fib[i-2]; 
+        std::cout << fib[i] << std::endl;
+
+
+    }
+}
