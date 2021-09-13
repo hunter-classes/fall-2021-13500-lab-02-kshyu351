b0044ac9121c482864c901fa176bea62aa1bca83
Kirsten Shyu <kirsten.shyu00@myhunter.cuny.edu>
2021-09-13 00:07:57 -0400

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+/*
+Author: Kirsten Shyu
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 2B
+
+Write a program print-interval.cpp that asks the user 
+to input two integers L and U (representing the lower 
+and upper limits of the interval), and print out all 
+integers in the range L ≤ i < U separated by spaces.
+*/
+
+#include <iostream>
+
+#include "funcs.h"
+
+int main() {
+
+    print_interval(-5, 10);
+
+}
+
+
+
+    
+
